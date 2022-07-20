d0e48e985f0e2203eea5f5348802407318b7d969
Eric Melo <ericmelomp@gmail.com>
2022-07-20 11:17:49 -0300

    Criação do Shield Stats no README e códigos comentados

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "document_db" {
+  description = "Defining the types of arguments in the map of object"
   type = map(object({
 
     cluster_identifier = string
@@ -7,13 +8,13 @@ variable "document_db" {
     master_username    = string
     skip_final_snapshot = bool
   }))
-
+#You can create the tf.vars file instead of using "default" for the values
   default = {
     "document_db_test" = {
       cluster_identifier = "docdb-cluster-demo"
       engine_version     = "4.0"
-      master_password    = "Pa55w.rd"
-      master_username    = "root"
+      master_password    = "?"
+      master_username    = "?"
       skip_final_snapshot = true
     }
   }
