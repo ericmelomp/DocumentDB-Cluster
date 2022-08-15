89cd0fbe237857dda82fe629b4b889c2cdbdc798
Eric Melo <ericmelomp@gmail.com>
2022-08-15 16:11:11 -0300

    Inserção tfvars

---
+++ input.tfvars
@@ -0,0 +1,9 @@
+document_db = {
+  "dcdb" = {
+    cluster_identifier  = "docdb-cluster-demo"
+    engine_version      = "4.0"
+    master_password     = "Pa55w.rd"
+    master_username     = "root"
+    skip_final_snapshot = true
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,20 +2,10 @@ variable "document_db" {
   description = "Defining the types of arguments in the map of object"
   type = map(object({
 
-    cluster_identifier = string
-    engine_version     = string
-    master_password    = string
-    master_username    = string
+    cluster_identifier  = string
+    engine_version      = string
+    master_password     = string
+    master_username     = string
     skip_final_snapshot = bool
   }))
-#You can create the tf.vars file instead of using "default" for the values
-  default = {
-    "document_db_test" = {
-      cluster_identifier = "docdb-cluster-demo"
-      engine_version     = "4.0"
-      master_password    = "?"
-      master_username    = "?"
-      skip_final_snapshot = true
-    }
-  }
 }
